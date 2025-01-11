3dc8d55de8af23ee0d14c88b3843f33b972d52be
codepawan-dev <pawansdevadiga1@gmail.com>
2025-01-11 23:27:04 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -2,29 +2,34 @@ let home = document.getElementById('home')
 let about = document.getElementById('about');
 let skill = document.getElementById('skill');
 let contact = document.getElementById('contact');
+let cont = document.getElementById('cont');
 
+// Buttons
 let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
 let btn3 = document.getElementById('btn3');
 let btn4 = document.getElementById('btn4');
 
+// Navbars
 let nav1 = document.getElementById('nav1');
 let nav2 = document.getElementById('nav2');
 let nav3 = document.getElementById('nav3');
 let nav4 = document.getElementById('nav4');
 
+// Display
 let disp = document.getElementById('disp');
 let loading = document.getElementById('loading');
 
+// loading screen
 let lding1 = document.getElementById('lding1');
 let lding2 = document.getElementById('lding2');
 let lding3 = document.getElementById('lding3');
 let lding4 = document.getElementById('lding4');
 let ld_title = document.getElementById('ld-title');
 
-disp.style.display="none";
-ld_title.style.display="block";
 
+disp.style.display = "none";
+ld_title.style.display = "block";
 ld_title.textContent = 'Loading';
 
 setTimeout('ld_title.textContent = "Loading•.."', 1000);
@@ -41,36 +46,21 @@ setTimeout('lding4.style.border = "4px solid black"', 3000);
 setTimeout('loading.style.display="none"', 4000);
 setTimeout('disp.style.display="block"', 4000);
 setTimeout('ld_title.style.display="none"', 4000);
+setTimeout('cont.style.display="flex"', 4000);
 
 
-function large1() {
+nav1.addEventListener('click', function run() {
     btn1.style.display = 'block';
     about.style.display = 'none';
     skill.style.display = 'none';
     contact.style.display = 'none';
-
     nav2.style.display = 'none';
     nav3.style.display = 'none';
     nav4.style.display = 'none';
+    document.querySelector('.card').classList.toggle('dark');
+});
 
-    if (home.style.width != "1200px" && home.style.height != "360px") {
-        home.style.width = '1200px';
-        home.style.height = '360px';
-        about.style.width = '684px';
-        about.style.height = '280px';
-        skill.style.width = '684px';
-        skill.style.height = '280px';
-        contact.style.width = '684px';
-        contact.style.height = '280px';
-
-    }
-    else {
-        home.style.width = '684px';
-        home.style.height = '280px';
-    }
-}
-
-function large2() {
+nav2.addEventListener('click', function run() {
     btn2.style.display = 'block';
     home.style.display = 'none';
     skill.style.display = 'none';
@@ -79,25 +69,9 @@ function large2() {
     nav1.style.display = 'none';
     nav3.style.display = 'none';
     nav4.style.display = 'none';
+});
 
-
-    if (about.style.width != "1200px" && about.style.height != "360px") {
-        about.style.width = '1200px';
-        about.style.height = '360px';
-        home.style.width = '684px';
-        home.style.height = '280px';
-        skill.style.width = '684px';
-        skill.style.height = '280px';
-        contact.style.width = '684px';
-        contact.style.height = '280px';
-    }
-    else {
-        about.style.width = '684px';
-        about.style.height = '280px';
-    }
-}
-
-function large3() {
+nav3.addEventListener('click', function run() {
     btn3.style.display = 'block';
     home.style.display = 'none';
     about.style.display = 'none';
@@ -106,25 +80,9 @@ function large3() {
     nav1.style.display = 'none';
     nav2.style.display = 'none';
     nav4.style.display = 'none';
+});
 
-
-    if (skill.style.width != "1200px" && skill.style.height != "360px") {
-        skill.style.width = '1200px';
-        skill.style.height = '360px';
-        home.style.width = '684px';
-        home.style.height = '280px';
-        about.style.width = '684px';
-        about.style.height = '280px';
-        contact.style.width = '684px';
-        contact.style.height = '280px';
-    }
-    else {
-        skill.style.width = '684px';
-        skill.style.height = '280px';
-    }
-}
-
-function large4() {
+nav4.addEventListener('click', function large4() {
     btn4.style.display = 'block';
     home.style.display = 'none';
     about.style.display = 'none';
@@ -133,22 +91,7 @@ function large4() {
     nav1.style.display = 'none';
     nav2.style.display = 'none';
     nav3.style.display = 'none';
-
-    if (contact.style.width != "1200px" && contact.style.height != "360px") {
-        contact.style.width = '1200px';
-        contact.style.height = '360px';
-        home.style.width = '684px';
-        home.style.height = '280px';
-        about.style.width = '684px';
-        about.style.height = '280px';
-        skill.style.width = '684px';
-        skill.style.height = '280px';
-    }
-    else {
-        contact.style.width = '684px';
-        contact.style.height = '280px';
-    }
-}
+});
 
 btn1.addEventListener('click', function run() {
     home.style.width = '684px';
@@ -201,3 +144,19 @@ btn4.addEventListener('click', function run() {
     nav3.style.display = 'flex';
     nav4.style.display = 'flex';
 });
+
+
+// Date 
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let timings = new Date();
+let hour = timings.getHours();
+let minute = timings.getMinutes();
+let hours_minutes = `${hour}${minutes}`;
+
+function updateTime() {
+    hours.textContent = hour;
+    minutes.textContent = minute;
+}
+
+setInterval(updateTime, 1000);
